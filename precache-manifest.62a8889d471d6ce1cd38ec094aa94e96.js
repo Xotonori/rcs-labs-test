aa20c0ddb08d6b55ccb5318782fce26b051c3313
Xotonori <xotonori29011991@gmail.com>
2020-09-08 08:00:13 +0300

    Updates

---
+++ precache-manifest.62a8889d471d6ce1cd38ec094aa94e96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958dbf79fce64babde25004d01c9eea0",
+    "url": "/rcs-labs-test/index.html"
+  },
+  {
+    "revision": "7e97aeba59abcac99460",
+    "url": "/rcs-labs-test/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "554b797cd337869eae7b",
+    "url": "/rcs-labs-test/static/css/main.681a07ba.chunk.css"
+  },
+  {
+    "revision": "7e97aeba59abcac99460",
+    "url": "/rcs-labs-test/static/js/2.087f5657.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/rcs-labs-test/static/js/2.087f5657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "554b797cd337869eae7b",
+    "url": "/rcs-labs-test/static/js/main.8ef1b241.chunk.js"
+  },
+  {
+    "revision": "4b66f16200b9d3728557",
+    "url": "/rcs-labs-test/static/js/runtime-main.daf2a0a1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rcs-labs-test/precache-manifest.62a8889d471d6ce1cd38ec094aa94e96.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rcs-labs-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
